2b503cac4517487bc0a27af352a531247c702227
BrunoSMoreiraext <bruno.2015.moreiraext@gmail.com>
2026-02-06 20:37:22 -0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useState} from 'react';
-import './modal.css'
+import './Modal.css'
 
 export default function Modal({isOpen, onClose, onSave, tarefaParaEditar}) {
 
